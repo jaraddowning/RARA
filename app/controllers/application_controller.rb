2aab4336dcb9e393393602c753576a90ab17b40e
Jarad Downing <jaraddowning@gmail.com>
2011-07-11 11:17:31 -0400

    password reset link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include Hobo::Controller::AuthenticationSupport
-  before_filter :except => [:login, :forgot_password] do
-     login_required unless User.count == 0
+  before_filter :except => [:login, :signup, :activate, :do_activate, 
+    :do_signup, :forgot_password, :reset_password, 
+    :do_reset_password] do
+      login_required unless User.count == 0
   end
+
 end
