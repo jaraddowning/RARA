5db9ef857e0ca2a6547f675baf1c88a28856de43
bentbrewer <bentbrewer@ymail.com>
2012-03-05 21:12:50 -0500

    data update and interview card

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -3,7 +3,7 @@ class Interview < ActiveRecord::Base
   hobo_model # Don't put anything above this
 
   fields do
-    name        :string
+    name        :string, :name => true
     affiliation :string
     position    :string
     contact     :text
